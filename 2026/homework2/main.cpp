e1337470956967f92836abc4a86bd95153f1984e
Josh1804 <Joshua.rodriguez@yale.edu>
2026-03-03 01:02:03 -0500

    finished/able to push

--- 2026/homework2/main.cpp
+++ 2026/homework2/main.cpp
@@ -70,7 +70,6 @@ public:
     {
       vec_ptr->resize(capacity);
     }
-    std::cout << "Buffer acquired by: " << omp_get_thread_num() << std::endl;
     return vec_ptr;
   }
 
@@ -88,7 +87,6 @@ public:
       oldHead = head.load();
       newHead->next = oldHead;
     } while (!head.compare_exchange_weak(oldHead, newHead));
-    std::cout << "Buffer released by: " << omp_get_thread_num() << std::endl;
     // ----------------------
   }
 };
@@ -101,14 +99,7 @@ AtomicBufferPool pool;
 
 void seq_merge(int *A, int nA, int *B, int nB, int *C)
 {
-  // std::cout << "Merging by: " << omp_get_thread_num() << " nA " << nA << " A " << A << " nB " << nB << std::endl;
-  std::cout << "Merging by: " << omp_get_thread_num() << " A[nA-1] " << A[nA-1] << " B[nB-1] " << B[nB-1] << " A[0] " << A[0] << std::endl;
   std::merge(A, A + nA, B, B + nB, C);
-  // if (nA + nB == 2048){
-  //   for (int i = 0; i <= nA + nB - 1; i++){
-  //     std::cout << "T[" << i << "] = " << T[i] << std::endl;
-  //   }
-  // }
 }
 
 // TODO: Implement the Divide-and-Conquer Parallel Merge
@@ -119,8 +110,7 @@ void parallel_binary_merge(int *A, int sA, int tA, int *B, int sB, int tB, int *
   int nB = tB - sB + 1; 
   // 1. Base Case (use seq_merge)
   if (nA + nB < SERIAL_THRESHOLD){
-    // std::cout << "Merging by: " << omp_get_thread_num() << " nA " << nA << " sA " << sA << " nB " << nB << " sB " << sB << std::endl;
-    seq_merge(A + sA, nA, B + sB, nB, C);
+    seq_merge(A + sA, nA, B + sB, nB, C + sC);
     return;
   }
 
@@ -146,85 +136,54 @@ void parallel_binary_merge(int *A, int sA, int tA, int *B, int sB, int tB, int *
   }
 
   if (nA <= 0 || nB <= 0){
+    // If one side is empty, copy the other side into C.
+    if (nA <= 0 && nB > 0){
+      for (int i = 0; i < nB; ++i){
+        C[sC + i] = B[sB + i];
+      }
+    } 
+    else if (nB <= 0 && nA > 0){
+      for (int i = 0; i < nA; ++i){
+        C[sC + i] = A[sA + i];
+      }
+    }
     return;
   }
 
   // 3. Find Median of A
-  // floors answer // MAYBE MINUS ONE 
+  // floors answer MAYBE MINUS ONE 
   int midA = sA + nA/2 - 1; // start plus half of the size 
   int medianA = A[midA];
 
-  // 4. Binary Search Median in B
-  int j = -1;
-  int top = tB; 
+  // 4. Binary Search Median in B 
   int bottom = sB;
-  // could be parallelized 
-  while(bottom < top){
+  int top = tB + 1; // one past end
+
+  while (bottom < top){
     int mid = bottom + (top - bottom)/2;
-    // std::cout << "Merging by: " << omp_get_thread_num() << " mid " << mid << " Bottom " << bottom << " top " << top << std::endl;
-    if (B[mid] == medianA){
-      j = mid;
-      // std::cout << "Merging by: " << omp_get_thread_num() << " mid " << mid << " Bottom " << bottom << " top " << top << std::endl;
-      // std::cout << "Merging by: " << omp_get_thread_num() << " mid should end now "<< i << std::endl;
-      break;
-    }
+
     if (B[mid] < medianA){
       bottom = mid + 1;
-      j = bottom;
-      // std::cout << "Merging by: " << omp_get_thread_num() << " mid " << mid << " Bottom " << bottom << " top " << top << std::endl;
-      // std::cout << "Merging by: " << omp_get_thread_num() << " B[mid] " << B[mid] << " median " << medianA << std::endl;
+    } 
+    else {
+      top = mid;
     }
-    if (B[mid] > medianA) {
-      j = top;
-      top = mid - 1;
-      // std::cout << "Merging by: " << omp_get_thread_num() << " mid " << mid << " Bottom " << bottom << " top " << top << std::endl;
-      // std::cout << "Merging by: " << omp_get_thread_num() << " mid should end now "<< i << std::endl;
-    }
-    if (bottom == top){
-      j = top;
-      // std::cout << "Merging by: " << omp_get_thread_num() << " mid should end now "<< i << std::endl;
-      break;
-    }
-    // std::cout << "Merging by: " << omp_get_thread_num() << " mid " << mid << " Bottom " << bottom << " top " << top << std::endl;
-    // std::cout << "Merging by: " << omp_get_thread_num() << " sB " << sB << " tB " << tB << std::endl;
-    // std::cout << "Merging by: " << omp_get_thread_num() << " j " << j << std::endl;
   }
-  // if (j == -1){
-  //   if (bottom == top){
-  //     j = bottom;
-  //   }
-    // std::cout << "Merging by: " << omp_get_thread_num() << " uhm... idk how this happened " << std::endl;
-  //   // std::cout << "Merging by: " << omp_get_thread_num() << " broken: Bottom " << bottom << " top " << top << std::endl;
-  //   // std::cout << "Merging by: " << omp_get_thread_num() << " sB " << sB << " tB " << tB  << " j " << j << std::endl;
-  //   // std::cout << "Merging by: " << omp_get_thread_num() << " B[mid] " << B[mid] << " median " << medianA << std::endl;
-  // }
-
+  int j = top; // may equal tB+1 if all elements < medianA
+  // int j = std::lower_bound(B + sB, B + tB + 1, medianA) - B;
   // 5. Place Median in C
-  int midC = midA + j;
-  // std::cout << "Merging by: " << omp_get_thread_num() << " sB " << sB << " tB " << tB << std::endl;
-  // std::cout << "Merging by: " << omp_get_thread_num() << " sA " << sA << " tA " << tA << std::endl;
-  // std::cout << "Merging by: " << omp_get_thread_num() << " midA " << midA << " j " << j << std::endl;
-  // std::cout << "Merging by: " << omp_get_thread_num() << " midC " << midC << " C[midC] " << C[midC] << std::endl;
+  int midC = sC + (midA - sA) + (j - sB);
+
   C[midC] = medianA;
   // 6. Spawn 2 Recursive Tasks (Left and Right)
-  #pragma omp task 
+  // Spawn left and right tasks. allow recursive call to handle empty ranges.
+  #pragma omp task
   {
-    if (sB < j){
-      parallel_binary_merge(A, sA, midA - 1, B, sB, j - 1, C, sC, midC);
-    }
-    // else {
-    //   std::cout << "Merging by: " << omp_get_thread_num() << " sB " << sB << " tB " << tB << std::endl;
-    // }
+    parallel_binary_merge(A, sA, midA - 1, B, sB, j - 1, C, sC, midC - 1);
   }
-  #pragma omp task 
+  #pragma omp task
   {
-    if (tB > j + 1){
-      // uses pointer arithmetic to move array up to midA and j 
-      parallel_binary_merge(A, midA, tA, B, j, tB, C, midC + 1, tC);
-    }
-    // else {
-    //   std::cout << "Merging by: " << omp_get_thread_num() << " sB " << sB << " tB " << tB << std::endl;
-    // }
+    parallel_binary_merge(A, midA + 1, tA, B, j, tB, C, midC + 1, tC);
   }
 
   // 7. Wait
@@ -290,34 +249,18 @@ void mergesort_4way(int *arr, int n)
   // --- TODO: YOUR CODE HERE ---
   #pragma omp task 
   {
-    std::cout << "P1 and P2 merging by: " << omp_get_thread_num() << std::endl;
     parallel_binary_merge(p1, 0, s1 - 1, p2, 0, s2 - 1, T, 0, s1 + s2 - 1);
-    std::cout << "P1 and P2 binary merged by: " << omp_get_thread_num() << std::endl;
-    // for (int i = s2 + s1 - 1; i >= 0 ; i--){
-    //   std::cout << "T[" << i << "] = " << T[i] << std::endl;
-    // }
   }
   #pragma omp task 
   {
-    std::cout << "P3 and P4 merging by: " << omp_get_thread_num() << std::endl;
     parallel_binary_merge(p3, 0, s3 - 1, p4, 0, s4 - 1, T_mid, 0, s3 + s4 - 1);
-    std::cout << "P3 and P4 binary merged by: " << omp_get_thread_num() << std::endl;
-    // for (int i = s3 + s4 - 1; i >= 0 ; i--){
-    //   std::cout << "T_mid[" << i << "] = " << T_mid[i] << std::endl;
-    // }
-    // std::cout << "s3: " << s3 << " s4: " << s4 << std::endl;
   }
   
   #pragma omp taskwait
   // ----------------------
 
   // 4. Final Merge: Left+Right -> Original Array
-  std::cout << "T and T_mid merging by: " << omp_get_thread_num() << std::endl;
   parallel_binary_merge(T, 0, s1 + s2 - 1, T_mid, 0, s3 + s4 - 1, arr, 0, n - 1);
-  std::cout << "T and T_mid merging by: " << omp_get_thread_num() << std::endl;
-  for (int i = s1 + s2 + s3 - 5; i <= s2 + s2 + s3 + 10 ; i++){
-    std::cout << "arr[" << i << "] = " << arr[i] << std::endl;
-  }
   // 5. Cleanup
   pool.release_buffer(temp_vec);
 }
@@ -364,6 +307,7 @@ int main(int argc, char *argv[])
   double elapsed = end_time - start_time;
 
   std::sort(check.begin(), check.end());
+
   bool passed = (data == check);
 
   // Output formatted string for Python tester
